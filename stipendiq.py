1b1102fc324c12f4264206b61cd7ce6ba0d0737f
Stanislav <101987485+StanislavHristov@users.noreply.github.com>
2023-06-01 22:20:43 +0300

    Add files via upload

---
+++ stipendiq.py
@@ -0,0 +1,10 @@
+uspeh = float(input("Enter uspeh:"))
+MaxStip = int(input("Vyvedete max stipendiq:"))
+if uspeh >= 5.50:
+    print("Max stipendiq", MaxStip)
+elif 5.50 > uspeh >= 5.00:
+    print("70% ot Max stipendiq", MaxStip*0,7)
+elif 5.00 > uspeh >= 4.50:
+    print("50% ot Max stipendiq", MaxStip*0,5)
+else:
+    print("Ne poluchava stipendiq")
