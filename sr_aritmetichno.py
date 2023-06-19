f24b42d8a97a4b203d3f62fffdf71ba2a2a30c1d
Stanislav Hristov <101987485+StanislavHristov@users.noreply.github.com>
2023-06-19 23:06:14 +0300

    Update sr_aritmetichno.py

--- sr_aritmetichno.py
+++ sr_aritmetichno.py
@@ -1,7 +1,7 @@
-suma=0
-count=0
-for i in range (7,70):
-    if i % 3 ==0:
-        count+=1
-        suma+=i
-print("Sr. aritm.=",suma/count)
+suma = 0
+count = 0
+for i in range (7, 70):
+    if i % 3 == 0:
+        count += 1
+        suma += i
+print("Sr. aritm. = ", suma / count)
